68360e18faba78701278ebe8891729a653c6fe68
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2018-12-13 18:21:32 +0600

    added one more card

--- components/src/App.js
+++ components/src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
           <MovieContent name="The Front Runner" released={2018} img="https://amc-theatres-res.cloudinary.com/image/upload/f_auto,fl_lossy,h_600,q_auto,w_400/v1/amc-cdn/production/2/movies/57500/57501/PosterDynamic/66795.jpg"/>
           <MovieContent name="Mortal Engines" released={2018} img="https://amc-theatres-res.cloudinary.com/image/upload/f_auto,fl_lossy,h_600,q_auto,w_400/v1/amc-cdn/production/2/movies/52600/52559/PosterDynamic/43806.jpg"/>
           <MovieContent name="Dr. Seuss' The Grinch" released={2018} img="https://amc-theatres-res.cloudinary.com/image/upload/f_auto,fl_lossy,h_600,q_auto,w_400/v1/amc-cdn/production/2/movies/44500/44521/PosterDynamic/66594.jpg"/>
+          <MovieContent name="Dr. Seuss' The Grinch" released={2018} img="https://amc-theatres-res.cloudinary.com/image/upload/f_auto,fl_lossy,h_600,q_auto,w_400/v1/amc-cdn/production/2/movies/44500/44521/PosterDynamic/66594.jpg"/>
         </div>
 
     );
