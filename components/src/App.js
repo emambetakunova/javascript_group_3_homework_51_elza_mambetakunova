3e7b66f2dbb48fb6596b042078fd25420a11b0e8
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2018-12-14 01:01:38 +0600

    modified Sidebar Component

--- components/src/App.js
+++ components/src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import MovieContent from './components/MovieComponent/Movie';
 import Header from './components/HeaderComponent/Header';
 import './App.css';
+import Sidebar from "./components/SidebarComponent/Sidebar";
 
 class App extends Component {
     render() {
@@ -11,12 +12,21 @@ class App extends Component {
                     <Header home={'home'} about={'about'} movies={'movies'}/>
                 </div>
                 <div className="wrapper">
-                    <MovieContent name="Ralph 2" released={2018}
-                                  img="https://amc-theatres-res.cloudinary.com/image/upload/f_auto,fl_lossy,h_600,q_auto,w_400/v1/amc-cdn/production/2/movies/43000/43031/PosterDynamic/66364.jpg"/>
-                    <MovieContent name="Venom" released={2018}
-                                  img="https://amc-theatres-res.cloudinary.com/image/upload/f_auto,fl_lossy,h_600,q_auto,w_400/v1/amc-cdn/production/2/movies/53400/53391/PosterDynamic/46534.jpg"/>
-                    <MovieContent name="Mortal Engines" released={2018}
-                                  img="https://amc-theatres-res.cloudinary.com/image/upload/f_auto,fl_lossy,h_600,q_auto,w_400/v1/amc-cdn/production/2/movies/52600/52559/PosterDynamic/43806.jpg"/>
+                        <MovieContent name="Ralph 2" released={2018}
+                                      img="https://amc-theatres-res.cloudinary.com/image/upload/f_auto,fl_lossy,h_600,q_auto,w_400/v1/amc-cdn/production/2/movies/43000/43031/PosterDynamic/66364.jpg"/>
+                        <MovieContent name="Venom" released={2018}
+                                      img="https://amc-theatres-res.cloudinary.com/image/upload/f_auto,fl_lossy,h_600,q_auto,w_400/v1/amc-cdn/production/2/movies/53400/53391/PosterDynamic/46534.jpg"/>
+                        <MovieContent name="Mortal Engines" released={2018}
+                                      img="https://amc-theatres-res.cloudinary.com/image/upload/f_auto,fl_lossy,h_600,q_auto,w_400/v1/amc-cdn/production/2/movies/52600/52559/PosterDynamic/43806.jpg"/>
+                    <div className="sidebar">
+                        <h4>Movie review: </h4>
+                        <Sidebar name="Ralph 2" released={2018}
+                                 review={'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'}/>
+                        <Sidebar name="Venom" released={2018}
+                                 review={'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'}/>
+                        <Sidebar name="Mortal Engines" released={2018}
+                                 review={'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'}/>
+                    </div>
                 </div>
             </div>
         );
--- components/src/components/MovieComponent/Movie.js
+++ components/src/components/MovieComponent/Movie.js
@@ -3,14 +3,14 @@ import './Movie.css';
 
 
 const MovieContent = (props) => {
-	return (
-					<div className="movieName">
-						<h4>Movie name: {props.name}</h4>
-						<p>Released: {props.released}</p>
-						<img src={props.img} alt=""/>
-					</div>
+    return (
+        <div className="movieName">
+            <h4>Movie name: {props.name}</h4>
+            <p>Released: {props.released}</p>
+            <img src={props.img} alt=""/>
+        </div>
 
-		)
+    )
 };
 
 export default MovieContent;
